b9695191d850bd6138f7a9f6e794c805e372fc82
Tomas <tomas@2a:69:cf:16:00:b1.speedport.ip>
2025-12-30 10:45:07 +0100

    Fix: Hardcode auth domains (remove env var)

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_ISSUER_URL ?? "https://helping-buffalo-10.clerk.accounts.dev",
+      domain: "https://helping-buffalo-10.clerk.accounts.dev",
       applicationID: "convex",
     },
     {
